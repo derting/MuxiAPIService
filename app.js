cf896796c8c98147792a52877e74e268c30f2825
Derting <derting@DT-MBP.local>
2018-06-04 22:39:46 +0800

    (1) update api url to json. (2) add hymnary api and (3) upload first jsondata for hymnary.

--- app.js
+++ app.js
@@ -1,19 +1,42 @@
-
 const express = require('express');
-
 const app = express();
+var bodyParser = require('body-parser');
 
-app.get('/', (req, res) => {
+var hymnary = require('./src/hymnary.json');
+
+// configure app to use bodyParser()
+// this will let us get the data from a POST
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// set our port
+var port = process.env.PORT || 8080;        
+
+// ROUTERS FOR OUR API
+var router = express.Router();
+
+// REGISTER OUR ROUTES -------------------------------
+app.use('/api',router);
+
+// accessed at GET http://127.0.0.1:8080/api)
+router.get('/', (req, res) => {
   res.status(200).send('Muxi Project').end();
 });
 
-app.get('/muxi/version', (req, res) => {
-  res.status(200).send('V1.0.0').end();
+// api/muxi/version
+router.get('/muxi/version', (req, res) => {
+  //res.status(200).send('V1.0.0').end();
+  res.status(200).json({version: 'V1.0.1'}).end();
 });
 
-const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}`);
+// api/muxi/hymnary
+router.get('/muxi/hymnary', (req, res) => {
+  //res.status(200).send('V1.0.0').end();
+  res.status(200).send(hymnary).end();
+});
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`);
   console.log('Press Ctrl+C to quit.');
 });
 

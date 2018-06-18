d3a4d10c84c65fd97e01335783b251ebb7ed25d5
Derting <derting@DT-MBP.local>
2018-06-19 00:23:25 +0800

    Complete the function of Patch and Delete.

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 var bodyParser = require('body-parser');
+const fs = require('fs');
 
 var hymnary_1 = require('./src/hymnary_1.json');
 var hymnary_2 = require('./src/hymnary_2.json');
@@ -43,24 +44,25 @@ router.get('/muxi/hymnary_2', (req, res) => {
 
 
 // <example>
+//  method: patch
 //  url: http://127.0.0.1:8080/api/muxi/hymnary/
 //  body: {"key":1,"file":"file1","content":"asd/nasfwr/nasf"}
 // </example>
 router.patch('/muxi/hymnary/', function (req, res) {
   console.log('[patch]hymnary ' + req.body.file + ", key:" + req.body.key);
-  
-  //node: 這裡可能需要debug，才知道如何update json file.
+  var result = UpdateFileHandler("M", req.body.file, req.body);
+  res.status(result.status).send({ "success": result.success, "message": result.message }).end();
+});
 
-  //fs.readFile('myjsonfile.json', 'utf8', function readFileCallback(err, data){
-  //    if (err){
-  //        console.log(err);
-  //    } else {
-  //    obj = JSON.parse(data); //now it an object
-  //    obj.table.push({id: 2, square:3}); //add some data
-  //    json = JSON.stringify(obj); //convert it back to json
-  //    fs.writeFile('myjsonfile.json', json, 'utf8', callback); // write it back 
-  //}});
-  res.status(200).send({ "success": true }).end();
+// <example>
+//  method: delete
+//  url: http://127.0.0.1:8080/api/muxi/hymnary/
+//  body: {"key":1,"file":"file1"}
+// </example>
+router.delete('/muxi/hymnary/', function (req, res) {
+  console.log('[delete]hymnary ' + req.body.file + ", key:" + req.body.key);
+  var result = UpdateFileHandler("D", req.body.file, req.body);
+  res.status(result.status).send({ "success": result.success, "message": result.message }).end();
 });
 
 app.listen(port, () => {
@@ -68,3 +70,43 @@ app.listen(port, () => {
   console.log('Press Ctrl+C to quit.');
 });
 
+
+//---------------
+
+function UpdateFileHandler(option, fileName, requestBody) {
+  fileURL = './src/' + fileName + '.json';
+  if (CheckFileExist(fileURL) == false) {
+    return { "status": 500, "success": false, "message": "No file." };
+  }
+
+  var fileContent = fs.readFileSync(fileURL, 'utf8');
+  fileContent = JSON.parse(fileContent);
+  if (option == "M") {
+    UpdateFileContent(fileContent, requestBody);
+  }
+  else if (option == "D") {
+    DeleteFileContent(fileContent, requestBody);
+  }
+
+  var fileContent_md = JSON.stringify(fileContent);
+  fs.writeFileSync(fileURL, fileContent_md, 'utf8');
+
+  return { "status": 200, "success": true, "message": "" };
+}
+
+function UpdateFileContent(fileContent, requestObj) {
+  var result = fileContent[requestObj.file].find(function () { return key == requestObj.key; });
+  result.content = requestObj.content;
+}
+
+function DeleteFileContent(fileContent, requestObj) {
+  var idx = fileContent[requestObj.file].findIndex(x => x.key == requestObj.key);
+  fileContent[requestObj.file].splice(idx, 1);
+}
+
+function CheckFileExist(path) {
+  if (fs.existsSync(path))
+    return true;
+  else
+    return false;
+}
